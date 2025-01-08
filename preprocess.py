8a936e84e2fcae1d598d420119c23c6094fcde87
reid efe <reidefe@gmail.com>
2025-01-08 15:57:14 +0000

    Update .gitignore to exclude .idea and .env

--- preprocess.py
+++ preprocess.py
@@ -3,12 +3,9 @@ import numpy as np
 
 
 def clean_data(df):
-    # Remove rows with missing values
     df = df.dropna()
-
     # Convert duration to seconds
     df['Average view duration'] = df['Average view duration'].apply(
         lambda x: sum(int(i) * 60 ** idx for idx, i in enumerate(reversed(str(x).split(':'))))
     )
-
     return df
